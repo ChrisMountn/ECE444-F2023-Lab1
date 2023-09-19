8a051f4ac1aa0a2d9b78d959994ccc3b8031f68e
Chris Mountain <ccallammountain@gmail.com>
2023-09-19 00:36:48 -0400

    activity 4-7

--- hello.py
+++ hello.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'asdhsi-98h-2gh0179bh02937hb092-9nbnu'
 
-@app.route('/')
+class EmailNameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
- return render_template('index.html',
- current_time=datetime.utcnow())
-
-@app.route('/user/<name>/')
-def user(name):
-    return render_template('user.html', name=name)
+    emailNameForm = EmailNameForm()
+    if emailNameForm.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != emailNameForm.name.data:
+            flash("Looks like you have changed your name!")
+        if "utoronto" not in emailNameForm.email.data:
+          flash("You have not submitted a U of T Email Address. Please submit a U of T Address.")
+        else:
+            session['name'] = emailNameForm.name.data
+            session['email'] = emailNameForm.email.data
+            return redirect(url_for('index'))
+    return render_template('index.html', emailnameform=emailNameForm, name=session.get('name'), email = session.get('email'))
 
 @app.errorhandler(404)
 def page_not_found(e):
--- readme.md
+++ readme.md
@@ -7,3 +7,11 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky.
 ![Screenshot of Commit](/screenshots/Activity2-App.JPG?raw=true)
 
 ![Screenshot of Commit](/screenshots/Activity3.JPG?raw=true)
+
+![Screenshot of Commit](/screenshots/Activity4-1.JPG?raw=true)
+
+![Screenshot of Commit](/screenshots/Activity4-2.JPG?raw=true)
+
+![Screenshot of Commit](/screenshots/Activity4-3.JPG?raw=true)
+
+![Screenshot of Commit](/screenshots/Activity4-4.JPG?raw=true)
